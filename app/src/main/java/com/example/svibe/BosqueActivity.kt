9856e2e16446101b5ed996b442ce1ea679e2ad93
gaita <gaitanjessica22@gmail.com>
2026-06-02 20:18:09 -0600

    Agregando contenido

--- app/src/main/java/com/example/svibe/BosqueActivity.kt
+++ app/src/main/java/com/example/svibe/BosqueActivity.kt
@@ -1,11 +1,13 @@
 package com.example.svibe
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Calendar
-import android.content.Intent
+import androidx.cardview.widget.CardView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.Calendar
 
 class BosqueImposibleActivity : AppCompatActivity() {
 
@@ -17,6 +19,8 @@ class BosqueImposibleActivity : AppCompatActivity() {
         val txtEstado = findViewById<TextView>(R.id.txtEstado)
         val txtHora = findViewById<TextView>(R.id.txtHoraCierre)
         val txtMantenimiento = findViewById<TextView>(R.id.txtMantenimiento)
+
+        val cardEstado = findViewById<CardView>(R.id.cardEstado)
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
 
@@ -55,13 +59,11 @@ class BosqueImposibleActivity : AppCompatActivity() {
 
                 R.id.ra -> {
 
-                    // Pendiente cuando crees la pantalla RA
                     true
                 }
 
                 R.id.perfil -> {
 
-                    // Pendiente cuando crees la pantalla Perfil
                     true
                 }
 
@@ -71,7 +73,7 @@ class BosqueImposibleActivity : AppCompatActivity() {
 
         val horaActual = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
 
-        // Horario: 8:00 AM - 4:00 PM
+        // Horario oficial: 8:00 AM - 4:00 PM
         if (horaActual in 8..15) {
 
             txtEstado.text = "Abierto hoy"
@@ -84,6 +86,20 @@ class BosqueImposibleActivity : AppCompatActivity() {
             txtHora.text = "Abre mañana a las 8:00 AM"
             txtMantenimiento.text = "Fuera de horario"
 
+        }
+
+        // Mostrar precios al tocar la tarjeta de estado
+        cardEstado.setOnClickListener {
+
+            val dialog = BottomSheetDialog(this)
+
+            val view = layoutInflater.inflate(
+                R.layout.bottom_sheet_precios,
+                null
+            )
+
+            dialog.setContentView(view)
+            dialog.show()
         }
     }
 }
